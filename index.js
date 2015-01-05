4c0f4c80398ef3655c44de0be2cf6eaf5331c199
hoovercj <hoovercj@iastate.edu>
2015-01-05 14:14:38 +0100

    Generic structure set up and working for Dropbox and IFTTT

--- index.js
+++ index.js
@@ -5,67 +5,78 @@ var cheerio = require('cheerio');
 var tough = require('tough-cookie');
 var app = express();
 
+var websites = {
+	dropbox: {
+		data: { confirm:'go', is_reset:'False' },
+		tokenTag: 'input[name=t]',
+		tokenAttr: 'value',
+		emailField: 'email',
+		tokenField: 't',
+		getUrl: 'https://www.dropbox.com/forgot',
+		postUrl: 'https://www.dropbox.com/forgot' 
+	}, ifttt: {
+		data: {},
+		tokenTag: 'meta[name=csrf-token]',
+		tokenAttr: 'content',
+		emailField: 'user[email]',
+		tokenField: 'authenticity_token',
+		getUrl: 'https://ifttt.com/forgot',
+		postUrl: 'https://ifttt.com/forgot'
+	}
+};
 
 // Initialization block
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-var email = "login@codyhoover.com";
-
-var dropboxUrl = "https://www.dropbox.com/forgot";
-var dropboxField = "email";
-
-var jar = request.jar();
 
 // Web Server Method Block
 app.get('/', function(req, resp) {  
-  resetIFTTT(email);
-  respond(resp, "Done");
+  //resetIFTTT(email);
+  resetWebsite(iftttEmail, websites['dropbox']);
+  resetWebsite(iftttEmail, websites['ifttt']);
+  respond(resp, "OK");
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-
-// IFTTT Helper Method Block
-var iftttUrl = "https://ifttt.com/forgot";
-var iftttEmailField = "user[email]";
-var iftttTokenField = "authenticity_token";
-
-function resetIFTTT(email) {
-	jar = request.jar();
-	makeIFTTTGetRequest(email);
+// Generic Website Method Block
+function resetWebsite(email, website) {
+	var cookieJar = request.jar();
+	console.log(JSON.stringify(website));
+	makeGetRequest(email, website, cookieJar);
 }
 
-function makeIFTTTGetRequest() {	
-	request({uri: iftttUrl, jar: jar}, function(err, resp, body) {
+function makeGetRequest(email, website, jar) {
+	request({uri: website.getUrl, jar: jar}, function(err, resp, body) {
 		if (err) {
 			console.log(err);
 		}
-		var iftttToken = getIFTTTToken(body);
-		makeIFTTTPostRequest(email, iftttToken);
+		var token = getToken(website, body);
+		console.log(token);
+		makePostRequest(email, website, jar, token);
 	});
 }
 
-function getIFTTTToken(html) {
-	$ = cheerio.load(html);
-	return $('meta[name=csrf-token]').attr("content");
-}
+function makePostRequest(email, website, jar, token) {
+	var data = website.data;
+	data[website.emailField] = email;
+	data[website.tokenField] = token;
 
-function makeIFTTTPostRequest(emailAddress, token) {
-
-	var data = {};
-	data[iftttEmailField] = emailAddress;
-	data[iftttTokenField] = token;
-
-	request.post({uri: iftttUrl, jar: jar, form: data}, function(err, resp, body) {
-		if (err) {
+	request.post({uri: website.postUrl, jar: jar, form: data}, function(err, resp, body) {
+		if(err) {
 			console.log(err);
 		}
-	});	
+	});
 }
 
+function getToken(website, html) {
+	$ = cheerio.load(html);
+	console.log(website.tokenTag + " -- " + website.tokenAttr);
+	return $(website.tokenTag).attr(website.tokenAttr);
+}
 
 // Generic Helper Method Block
 function  respond(response, message) {
